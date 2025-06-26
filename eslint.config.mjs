df550eaf175d768b0e67e86d048e2254b85da935
Rob Schuler <me@robomatic.us>
2025-06-25 17:14:35 -0700

    chore: migrate ESLint configuration from .eslintrc.json to eslint.config.mjs

---
+++ eslint.config.mjs
@@ -0,0 +1,19 @@
+import { FlatCompat } from '@eslint/eslintrc'
+
+const compat = new FlatCompat({
+  // import.meta.dirname is available after Node.js v20.11.0
+  baseDirectory: import.meta.dirname,
+})
+
+/**
+ * A custom ESLint configuration for libraries that use Next.js.
+ *
+ * @type {import("eslint").Linter.Config}
+ * */
+const eslintConfig = [
+  ...compat.config({
+    extends: ['next/core-web-vitals', 'next/typescript', 'prettier'],
+  }),
+]
+
+export default eslintConfig
